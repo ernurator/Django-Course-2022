7487353a7aa25481a9af9a21ceaad5184b2f42f9
Yernur <isenbaev121102ernur@gmail.com>
2022-03-10 00:00:53 +0600

    added todo list project

---
+++ todo_list/main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def todo_list_view(request):
+    pass
+    # return render(request, 'todo_list.jinja')
+
+
+def completed_todos_view(request):
+    pass
+    # return render(request, 'completed_todo_list.jinja')
